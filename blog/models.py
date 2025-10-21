5d673c17d275bf42328fbcd3da4c78a97a056cc3
dakoyak <ipyeongan760@gmail.com>
2025-10-21 15:22:52 +0900

    feat: 블로그 기능 추가 (미디어 파일 제외)

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True, null=True)
     content = models.TextField()
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     #author : 추후 작성 예정
-    head_image = models.URLField(max_length=200, blank=True, null=True)
+    head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d', null=True, blank=True)
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+#from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # def index(request):
@@ -12,18 +12,22 @@ from .models import Post
 #             'posts': posts,
 #         }
 #     )
+
 class PostList(ListView):
     model= Post
     ordering = '-pk'
     #template_name = 'blog/post_list.html'
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+class PostDetail(DetailView):
+    model = Post
+
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'post': post,
+#         }
+#     )
 
